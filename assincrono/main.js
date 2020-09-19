6ef4b2066e76c00be47443c587bdd8e718489d0f
WillianFarias <williansousafarias@gmail.com>
2020-09-19 17:19:37 -0300

    promises

--- assincrono/main.js
+++ assincrono/main.js
@@ -1,6 +1,7 @@
 //Utilizando AJAX, para que nao seja necessario recarregar a pag. toda vez que
 //buscar-se uma nova informacao no servidor
 
+/*
 var xhr = new XMLHttpRequest();
 
 xhr.open('GET', 'https://api.github.com/users/diego3g');
@@ -11,4 +12,35 @@ xhr.onreadystatechange = function () {
   if (xhr.readyState === 4) { 
     console.log(JSON.parse(xhr.responseText));
   }
+}*/
+
+var minhaPromise = function() {
+  return new Promise(function(resolve, reject) {
+    var xhr = new XMLHttpRequest();
+
+    xhr.open('GET', 'https://api.github.com/users/diego3g');
+    xhr.send(null);
+
+    //é necessario pois esta é uma requisicao assincrona
+    xhr.onreadystatechange = function () {
+      if(xhr.readyState === 4) {
+        if (xhr.status === 200) {
+          resolve(JSON.parse(xhr.responseText));
+        } else {
+          reject('Erro na requisição');
+        }
+      }
+    }
+  });
 }
+
+//Trabalhando com requisicoes assincronas com promises
+minhaPromise()
+  //resolve invoca um .then
+  .then(function(response){
+    console.log(response);
+  })
+  //reject invoca um .catch
+  .catch(function(error){
+    console.warn(error);
+  })

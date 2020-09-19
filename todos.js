09b599e3418f9fa7e305a60305c313543f3c0a77
WillianFarias <williansousafarias@gmail.com>
2020-09-19 16:22:33 -0300

    excluindo todos

--- todos.js
+++ todos.js
@@ -3,17 +3,48 @@ var inputElement = document.querySelector('#app input');
 var buttonElement = document.querySelector('#app button');
 
 var todos = [
-  {text: 'Fazer café', amount: 1}
+  'Fazer café'
 ];
 
 function renderTodos() {
+  //tudo que estiver dentro da html vai ficar como vazio
+  listElement.innerHTML = '';
+
   for (todo of todos) {
     var todoElement = document.createElement('li');
-    var todoText = document.createTextNode(todo.text);
+    var todoText = document.createTextNode(todo);
+
+    var linkElement = document.createElement('a');
+    linkElement.setAttribute('href', '#');
+
+    var posicao = todos.indexOf(todo);
+    linkElement.setAttribute('onclick', 'deleteTodo(' + posicao + ')');
+
+    var linkText = document.createTextNode('Excluir');
+
+    linkElement.appendChild(linkText);
 
     todoElement.appendChild(todoText);
-    listElement.appendChild(todoText);
+    todoElement.appendChild(linkElement);
+
+    listElement.appendChild(todoElement);
   }
 }
 
 renderTodos();
+
+function addTodo() {
+  var todoText = inputElement.value;
+
+  todos.push(todoText);
+  inputElement.value = '';
+
+  renderTodos();
+}
+
+buttonElement.onclick = addTodo;
+
+function deleteTodo(posicao) {
+  todos.splice(posicao, 1);
+  renderTodos();
+}
